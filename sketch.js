2f7124f5a7fc0f97b1893e5334a2f00fb00fe67b
pippayyy <80529128+pippayyy@users.noreply.github.com>
2021-09-14 17:07:59 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ centre_x = 100
 centre_y = 100
 
 function setup() {
-  createCanvas(displayWidth/2,displayHeight/2)
+  createCanvas(displayWidth,displayHeight)
   background(230)
   radius = height/2
 }
