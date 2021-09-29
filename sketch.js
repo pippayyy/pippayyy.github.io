1f30ec7c1df2bad46ad0b66d8edf10dc18639ff7
pippayyy <80529128+pippayyy@users.noreply.github.com>
2021-09-29 16:43:35 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ centre_x = 100
 centre_y = 100
 
 function setup() {
-  var MyCanvas = createCanvas(displayWidth,displayHeight);
+  var MyCanvas = createCanvas(displayWidth,displayHeight*0.8);
   MyCanvas.parent("DrawingContainer");
   background(230)
   radius = height/2
