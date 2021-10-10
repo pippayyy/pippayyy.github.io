f1fa5d17d73e2d0a7e3e61b8bc20ced398c1efd8
pippayyy <80529128+pippayyy@users.noreply.github.com>
2021-10-10 20:17:54 +0100

    Update colourPicker.js

--- colourPicker.js
+++ colourPicker.js
@@ -20,3 +20,24 @@ const pickr2 = new Pickr({
     }
   }
 });
+
+const pickr1 = new Pickr({
+  el: '#color-picker-1',
+  default: "303030",
+  components: {
+    preview: true,
+    opacity: true,
+    hue: true,
+
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      hsva: true,
+      cmyk: true,
+      input: true,
+      clear: true,
+      save: true
+    }
+  }
+});
