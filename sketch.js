63403159a2f54732c3161e1a2d940a76df8958a8
pippayyy <80529128+pippayyy@users.noreply.github.com>
2022-01-20 20:16:31 +0000

    Canvas scroll

--- sketch.js
+++ sketch.js
@@ -95,6 +95,21 @@ var btnCopy = $("#shareButton");
 //document.execCommand("copy");
 //});
 
+//Attempt Two - share button
+function copy(text, target) {
+  setTimeout(function () {
+    $("#copied_tip").remove();
+  }, 800);
+  $(target).append("<div class='tip' id='copied_tip'>Copied!</div>");
+  var input = document.createElement("input");
+  input.setAttribute("value", text);
+  document.body.appendChild(input);
+  input.select();
+  var result = document.execCommand("copy");
+  document.body.removeChild(input);
+  return result;
+}
+
 function createMetaTag() {
   let meta = createElement("meta");
   meta.attribute("name", "viewport");
